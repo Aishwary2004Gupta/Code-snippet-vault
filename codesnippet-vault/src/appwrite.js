f22bc771d673352b8e3ac16ce8fa527da9498ec0
Aishwary Gupta <aish042004@gmail.com>
2024-10-30 22:46:53 +0530

    Update appwrite.js

--- codesnippet-vault/src/appwrite.js
+++ codesnippet-vault/src/appwrite.js
@@ -2,8 +2,8 @@ import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client();
 client
-  .setEndpoint('YOUR_APPWRITE_ENDPOINT') // Your Appwrite endpoint
-  .setProject('YOUR_PROJECT_ID'); // Your project ID
+  .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite endpoint
+  .setProject('6721d41f001590bec8aa'); // Your project ID
 
 export const account = new Account(client);
 export const databases = new Databases(client);
